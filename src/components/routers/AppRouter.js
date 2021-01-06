ed6d5760cdc4a98254430b5258a3426d6daf5cc7
Jesús González Álvarez <51116828+JesusGonzalezA@users.noreply.github.com>
2021-01-06 13:40:50 +0100

    Implement routing

---
+++ src/components/routers/AppRouter.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route
+  } from "react-router-dom";
+  
+import { AuthRouter } from './AuthRouter';
+import { JournalScreen } from '../journal/JournalScreen';
+
+export const AppRouter = () => {
+    return (
+        <Router>
+            <div>
+                <Switch>
+                    <Route path="/auth" component={ AuthRouter } />
+                    <Route exact path="/" component={ JournalScreen } />
+                </Switch>
+            </div>
+        </Router>
+    )
+}
+
