24e06a995924bf266b864c47488d262f062f3d0a
Jesús González Álvarez <51116828+JesusGonzalezA@users.noreply.github.com>
2021-01-06 18:49:10 +0100

    Complete styles

--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -6,6 +6,28 @@ export const NoteScreen = () => {
         <div className="notes__main-content">
             
             <NotesAppBar />
+
+            <div className="notes__content mt-5">
+
+                <input 
+                    type="text"
+                    placeholder="Title"
+                    className="notes__title-input mb-5"
+                    autoComplete="off"
+                />
+
+                <textarea
+                    placeholder="What happened today?"
+                    className="notes__text-area mb-5"
+                />
+
+                <div className="notes__image">
+                    <img 
+                        src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"
+                        alt="" 
+                    />   
+                </div>
+            </div>
         </div>
     )
 }
