056de7d322350eac8be8dc6476b22ce8eb6535e5
Jesús González Álvarez <51116828+JesusGonzalezA@users.noreply.github.com>
2021-01-06 15:00:45 +0100

    Add styles to login screen

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -1,9 +1,53 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const LoginScreen = () => {
     return (
-        <div>
-            <h1>Login Screen</h1>
-        </div>
+        <>
+            <h1 className="auth__title mb-5">Login</h1>
+
+            <form>
+                <input 
+                    type="text"
+                    placeholder="E-mail"
+                    name="email"
+                    className="auth__input"
+                    autocomplete="off"
+                />
+
+                <input  
+                    type="password"
+                    placeholder="Password"
+                    name="password"
+                    className="auth__input"
+                />
+
+                <button type="submit" className="btn btn-primary btn-block">
+                    Login
+                </button>
+               
+                <div className="auth__social-networks">
+                    <p>Login with your fav social network</p>
+
+                    <div className="google-btn">
+                        <div className="google-icon-wrapper">
+                            <img 
+                                className="google-icon" 
+                                src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" 
+                                alt="Sign in with Google" 
+                            />
+                        </div>
+                        <p className="btn-text">
+                            <b>Sign in with Google</b>
+                        </p>
+                    </div>
+                </div>
+                
+                <Link to="auth/register">
+                    Not registered yet?
+                </Link>
+
+            </form>
+        </>
     )
 }
