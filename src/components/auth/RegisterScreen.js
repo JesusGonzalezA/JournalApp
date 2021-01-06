4e498939dba3b55a436dc9b23d3b4178b87b98bd
Jesús González Álvarez <51116828+JesusGonzalezA@users.noreply.github.com>
2021-01-06 17:17:50 +0100

    Register and login completed

--- src/components/auth/RegisterScreen.js
+++ src/components/auth/RegisterScreen.js
@@ -6,13 +6,12 @@ export const RegisterScreen = () => {
         <>
             <h1 className="auth__title mb-5">Sign up</h1>
 
-            <form autoComplete="off">
+            <form>
                 <input 
                     type="text"
                     placeholder="E-mail"
                     name="email"
                     className="auth__input"
-                    autoComplete="new-mail"
                 />
 
                 <input 
@@ -20,7 +19,6 @@ export const RegisterScreen = () => {
                     placeholder="Name"
                     name="name"
                     className="auth__input"
-                    autoComplete="none"
                 />
 
                 <input  
@@ -28,7 +26,6 @@ export const RegisterScreen = () => {
                     placeholder="Password"
                     name="password"
                     className="auth__input"
-                    autoComplete="none"
                 />
 
 
@@ -37,7 +34,6 @@ export const RegisterScreen = () => {
                     placeholder="Cofirm password"
                     name="password2"
                     className="auth__input"
-                    autoComplete="none"
                 />
 
                 <button type="submit" className="btn btn-primary btn-block mb-5">
@@ -48,13 +44,6 @@ export const RegisterScreen = () => {
                     Already registered?
                 </Link>
 
-                <input 
-  type="text" 
-  autoComplete="on" 
-  value="" 
-  style={{display: 'none', opacity: 0, position: 'absolute', left: '-100000px'}} 
-  readOnly={true}
-/>
             </form>
         </>
     )
