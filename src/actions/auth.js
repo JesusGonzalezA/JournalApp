c5b81f3de2b040c045c88db7c555de0dcb5faef8
Jesús González Álvarez <51116828+JesusGonzalezA@users.noreply.github.com>
2021-01-08 19:28:53 +0100

    Asyn func in redux

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,5 +1,17 @@
 import { types } from '../types/types'
 
+export const startLoginEmailPassword = (email, password) => {
+    return (dispatch) => {
+
+        setTimeout(() => {
+            
+            dispatch( login(123, 'Peter') );
+
+        }, 3500);
+
+    }
+}
+
 export const login = (uid, displayName) => ({
     type: types.login,
     payload: {
