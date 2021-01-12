58268ccad3ec6579757bc86c82398a6f4cc33107
Jesús González Álvarez <51116828+JesusGonzalezA@users.noreply.github.com>
2021-01-12 17:24:25 +0100

    Fetch the data from the store with useSelector

--- src/components/journal/Sidebar.js
+++ src/components/journal/Sidebar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { startLogout } from '../../actions/auth';
 import { JournalEntries } from './JournalEntries'
 
@@ -11,13 +11,16 @@ export const Sidebar = () => {
         dispatch( startLogout() );
     }
 
+    const authState = useSelector( state => state.auth );
+    const { name } = authState;
+
     return (
         <aside className="journal__sidebar">
             
             <div className="journal__sidebar-navbar mt-5">
                 <h3>
                     <i className="far fa-moon"></i>
-                    <span> Jesus</span>
+                    <span> { name }</span>
                 </h3>
 
                 <button className="btn" onClick={handleLogout}>
