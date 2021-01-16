f069bce7d054e7402b3bc6263cd2335ea8c62a52
Jesús González Álvarez <51116828+JesusGonzalezA@users.noreply.github.com>
2021-01-16 12:18:41 +0100

    Add testing db

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -9,9 +9,30 @@ const firebaseConfig = {
     storageBucket: "curso-react-2fdae.appspot.com",
     messagingSenderId: "138535508685",
     appId: "1:138535508685:web:62fe2e08341ef51181e784"
-  };
+};
 
-firebase.initializeApp(firebaseConfig);
+const firebaseConfigTesting = {
+  apiKey: "AIzaSyAo0RAUulqkXE3I4WG59yZU7Wj8U1lADUk",
+  authDomain: "react-testing-3698f.firebaseapp.com",
+  projectId: "react-testing-3698f",
+  storageBucket: "react-testing-3698f.appspot.com",
+  messagingSenderId: "683415470647",
+  appId: "1:683415470647:web:35a638b323480945baf5b2"
+};
+
+if ( process.env.NODE_ENV === 'test')
+{
+ 
+  //test
+  firebase.initializeApp(firebaseConfigTesting);
+
+} else {
+
+  // dev/prod
+  firebase.initializeApp(firebaseConfig);
+
+}
+
 
 const db = firebase.firestore();
 const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
