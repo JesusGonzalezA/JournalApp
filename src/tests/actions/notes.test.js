43818d62ccdd6dc7e099af502c4e953dfc2c7fdb
Jesús González Álvarez <51116828+JesusGonzalezA@users.noreply.github.com>
2021-01-16 12:23:59 +0100

    Test that I upload a new note to firebase

--- src/tests/actions/notes.test.js
+++ src/tests/actions/notes.test.js
@@ -1,6 +1,7 @@
 import configureStore from 'redux-mock-store';
 import thunk from 'redux-thunk'
 import { startNewNote } from '../../actions/notes';
+import { types } from '../../types/types';
  
 const middlewares = [thunk];
 const mockStore = configureStore(middlewares);
@@ -16,6 +17,28 @@ describe('Testing notes Actions', () => {
     test('should create a new Note (startNewNote)', async () => {
         
         await store.dispatch( startNewNote() );
+
+        const actions = store.getActions();
+        
+        expect( actions[0] ).toEqual({
+            type: types.notesActive,
+            payload: {
+                id: expect.any(String),
+                title: '',
+                body: '',
+                date: expect.any(Number)
+            }
+        });
+
+        expect( actions[1] ).toEqual({
+            type: types.notesAddNew,
+            payload: {
+                id: expect.any(String),
+                title: '',
+                body: '',
+                date: expect.any(Number)
+            }
+        })
     })
     
 })
