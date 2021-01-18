59625e880317052dcdaebbc91b543b8b45d77e83
Jesús González Álvarez <51116828+JesusGonzalezA@users.noreply.github.com>
2021-01-18 18:03:02 +0100

    Testing login finished

--- src/tests/components/LoginScreen.test.js
+++ src/tests/components/LoginScreen.test.js
@@ -7,6 +7,12 @@ import configureStore from 'redux-mock-store';
 import thunk from 'redux-thunk'
 import '@testing-library/jest-dom'
 import { MemoryRouter } from 'react-router-dom';
+import { startGoogleLogin, startLoginEmailPassword } from '../../actions/auth';
+
+jest.mock('../../actions/auth', () => ({
+    startGoogleLogin: jest.fn(),
+    startLoginEmailPassword: jest.fn()
+}))
 
 const middlewares = [thunk];
 const mockStore = configureStore(middlewares);
@@ -18,7 +24,8 @@ const inititalState = {
         msgError: null
     }
 };
-let store = mockStore( inititalState )
+let store = mockStore( inititalState );
+store.dispatch = jest.fn();
 
 const wrapper = mount( 
     <Provider store={store}>
@@ -31,11 +38,50 @@ const wrapper = mount(
 describe('Testing LoginScreen Component', () => {
     
     beforeEach(() => {
-        store = mockStore( inititalState )
+        store = mockStore( inititalState );
+        jest.clearAllMocks();
     });
 
     test('should match the snapshot', () => {
         expect( wrapper ).toMatchSnapshot();
+    })
+
+    test('should dispatch startGoogleSignIn', () => {
+
+        wrapper.find('.google-btn').prop('onClick')();
+
+        expect( startGoogleLogin ).toHaveBeenCalled();
+    })
+
+    test('should not call login when the form is not correct', () => {
+        
+        wrapper.find('form').simulate('submit'), {
+            preventDefault: () => {}
+        }
+
+        expect( startLoginEmailPassword ).not.toHaveBeenCalled();
+    })
+
+    test('should call login with user and password', () => {
+        wrapper.find('input[name="email"]').simulate('change', {             
+            target: {
+                name: 'email',                 
+                value: 'jesus@jesus.com'
+            }         
+        });
+
+        wrapper.find('input[name="password"]').simulate('change', {             
+            target: {
+                name: 'password',                 
+                value: '123456'
+            }         
+        });
+
+        wrapper.find('form').simulate('submit'), {
+            preventDefault: () => {}
+        }
+
+        expect( startLoginEmailPassword ).toHaveBeenCalledWith('jesus@jesus.com', '123456');
     })
     
 })
