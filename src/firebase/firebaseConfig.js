63d71f4dde8e62a88c975c367f49767ebbc9918c
Jesús González Álvarez <51116828+JesusGonzalezA@users.noreply.github.com>
2021-01-18 12:07:00 +0100

    Add env variables

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -3,36 +3,16 @@ import 'firebase/firestore'
 import 'firebase/auth'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBE8-0kBjB0mALKmGn6YXkQgF6ITNiH67U",
-    authDomain: "curso-react-2fdae.firebaseapp.com",
-    projectId: "curso-react-2fdae",
-    storageBucket: "curso-react-2fdae.appspot.com",
-    messagingSenderId: "138535508685",
-    appId: "1:138535508685:web:62fe2e08341ef51181e784"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
 };
-
-const firebaseConfigTesting = {
-  apiKey: "AIzaSyAo0RAUulqkXE3I4WG59yZU7Wj8U1lADUk",
-  authDomain: "react-testing-3698f.firebaseapp.com",
-  projectId: "react-testing-3698f",
-  storageBucket: "react-testing-3698f.appspot.com",
-  messagingSenderId: "683415470647",
-  appId: "1:683415470647:web:35a638b323480945baf5b2"
-};
-
-if ( process.env.NODE_ENV === 'test')
-{
  
-  //test
-  firebase.initializeApp(firebaseConfigTesting);
-
-} else {
-
-  // dev/prod
-  firebase.initializeApp(firebaseConfig);
-
-}
-
+//test
+firebase.initializeApp(firebaseConfig);
 
 const db = firebase.firestore();
 const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
