9f11327626081cd74a7d699412c95c8f981633ed
Jesús González Álvarez <51116828+JesusGonzalezA@users.noreply.github.com>
2021-01-18 16:19:38 +0100

    Test async actions in auth

--- src/tests/actions/auth.test.js
+++ src/tests/actions/auth.test.js
@@ -1,8 +1,23 @@
-import { login, logout } from "../../actions/auth"
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk'
+import '@testing-library/jest-dom'
+
+import { login, logout, startLoginEmailPassword, startLogout } from "../../actions/auth"
 import { types } from "../../types/types"
+
+
+const middlewares = [thunk];
+const mockStore = configureStore(middlewares);
+
+const inititalState = {};
+let store = mockStore( inititalState )
 
 describe('Testing auth actions', () => {
     
+    beforeEach(() => {
+        store = mockStore( inititalState )
+    });
+
     test('login and logout should create and clear the user', () => {
         
         expect ( logout() ).toEqual({
@@ -17,5 +32,35 @@ describe('Testing auth actions', () => {
             }
         })
     })
+
+    test('should start the login', async () => {
+        
+        await store.dispatch( startLogout() );
+
+        const actions = store.getActions();
+
+        expect( actions[0] ).toEqual({
+            type: types.logout
+        });
+        expect( actions[1] ).toEqual({
+            type: types.notesLogoutCleaning
+        })
+    });
+
+    test('should start Login with Email and Pass', async () => {
+        await store.dispatch( startLoginEmailPassword('test@testing.com', '123456') );
+
+        const actions = store.getActions();
+
+        expect( actions[1] ).toEqual({
+            type: types.login,
+            payload: {
+                uid: expect.any(String),
+                displayName: null
+            }
+        });
+    })
+    
+    
     
 })
